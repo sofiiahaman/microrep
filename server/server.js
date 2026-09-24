a90385cf904bc91c14185c702e6f326c81773470
sofiiahaman <sofiia.haman@gmail.com>
2026-09-24 20:20:25 +0300

    feat(db): setup prisma ORM with postgresql adapter and scripts

--- server/server.js
+++ server/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+require('dotenv/config');
+
+const prisma = require('./lib/prisma');
 
 const app = express();
 const PORT = Number(process.env.PORT) || 5001;
@@ -9,6 +12,14 @@ app.get('/', (req, res) => {
   res.send('Hello, MicroRep server!');
 });
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const shutdown = async () => {
+  await prisma.$disconnect();
+  server.close(() => process.exit(0));
+};
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
